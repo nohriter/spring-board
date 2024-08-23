016a98f819538973338006462aae3d805db697f6
nohriter <nohriter@gmail.com>
2024-08-23 10:52:59 +0900

    :bug: JpaAuditingConfig 파일 추가

--- src/main/java/com/nori/springboard/service/KakaoMemberInfoResponse.java
+++ src/main/java/com/nori/springboard/service/KakaoMemberInfoResponse.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nori.springboard.entity.Member;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
