fb0c4768421cd55c7fde37b8b1f1612d5e000b6e
nohriter <nohriter@gmail.com>
2024-08-23 14:10:51 +0900

    :wrench: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category (category_type)
+VALUES ('GENERAL');
+
+INSERT INTO category (category_type)
+VALUES ('QUESTION');
+
+INSERT INTO category (category_type)
+VALUES ('INFORMATION');
