57e9fb2e5b770e748fd8cf6708673ed13b163610
nohriter <nohriter@gmail.com>
2024-09-09 14:18:55 +0900

    :rocket: healthcheck api 추가

--- src/main/java/com/nori/springboard/controller/HomeController.java
+++ src/main/java/com/nori/springboard/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.nori.springboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,12 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		return "redirect:/board/view?category=all&page=1";
+	}
+
+	@ResponseBody
+	@GetMapping("/healthcheck")
+	public String healthcheck() {
+		return "OK";
 	}
 
 }
